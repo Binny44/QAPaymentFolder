a11c5c4397db12750020b909826080d65c2d5f7c
BinnyJ <binnyjohn39@gmail.com>
2020-11-01 22:12:37 -0500

    Billing test case

---
+++ src/paymentSystem/billingOne.java
@@ -0,0 +1,8 @@
+package paymentSystem;
+
+public class billingOne {
+    public static void main(String[] args) {
+        System.out.println("Binny is Mulesoft student");
+        System.out.println("Java is fun");
+    }
+}
